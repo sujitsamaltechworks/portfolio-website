13ab6c666600da0663b4cbbcf6f4296d4ad8c441
Sujit Samal <49195799+sujitsamaltechworks@users.noreply.github.com>
2022-09-20 16:35:43 +0530

    updated portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
+import IMG1 from "../../assets/weather-app.png";
 import IMG2 from "../../assets/portfolio2.jpg";
 import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
@@ -9,7 +9,7 @@ import IMG5 from "../../assets/portfolio5.png";
 import IMG6 from "../../assets/portfolio6.jpg";
 
 const projects = [
-  { image: IMG1, title: "Basic Form App" },
+  { image: IMG1, title: "Weather App", gitHub:"https://github.com/sujitsamaltechworks/portfolio-website", liveDemo: "https://thriving-pika-49efa6.netlify.app/" },
   { image: IMG2, title: "Basic Todo App" },
   { image: IMG3, title: "Basic Table App" },
   { image: IMG4, title: "Basic Redux App" },
@@ -32,10 +32,10 @@ const Portfolio = () => {
               </div>
               <h3>{item.title}</h3>
               <div className="portfolio__item-cta">
-                <a href="https://github.com" className="btn" target="_blank">
+                <a href={item.gitHub} className="btn" target="_blank">
                   Github
                 </a>
-                <a href="https://google.com" className="btn" target="_blank">
+                <a href={item.liveDemo} className="btn" target="_blank">
                   Live Demo
                 </a>
               </div>
